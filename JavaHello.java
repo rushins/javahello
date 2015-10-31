a60adabf58e75072d297751358f354c1965e102a
rushins <ns.rushi@gmail.com>
2015-10-31 14:19:45 -0700

    Create JavaHello.java

---
+++ JavaHello.java
@@ -0,0 +1,8 @@
+Public class JavaHello
+{
+        Public static void main (string args[])
+        {
+                System.out.printin(“java hello ”);
+                System.out.pringin(“Hello Agin”);
+        }
+}
