42be19a91762adcd48a2f33320ed54f0744779d6
rushins <ns.rushi@gmail.com>
2015-10-31 14:21:48 -0700

    Update JavaHello.java

--- JavaHello.java
+++ JavaHello.java
@@ -1,6 +1,6 @@
 Public class JavaHello
 {
-        Public static void main (string args[])
+        Public static void main (string [] args);
         {
                 System.out.printin(“java hello ”);
                 System.out.pringin(“Hello Agin”);
